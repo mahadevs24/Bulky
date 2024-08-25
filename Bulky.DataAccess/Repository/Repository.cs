d547fabe15e5c072209a501ab041f0fe0b05d4ed
mahadevs24 <mahadevshirodkar2@gmail.com>
2024-08-25 18:04:36 +0530

    Add project files.

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> set;
+        public Repository(ApplicationDbContext dbContex) : base()
+        {
+            _dbContext = dbContex;
+            set = _dbContext.Set<T>();
+        }
+        void IRepository<T>.Add(T Entity)
+        {
+            set.Add(Entity);
+        }
+
+        T IRepository<T>.Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = set;
+            query= query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll()
+        {
+            IQueryable<T> query = set;
+            //query = query.Where(filter);
+            return query.ToList();
+        }
+
+        void IRepository<T>.Remove(T Entity)
+        {
+            set.Remove(Entity);
+        }
+
+        void IRepository<T>.RemoveRange(IEnumerable<T> Entity)
+        {
+            set.RemoveRange(Entity);
+        }
+    }
+}
---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,62 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //private readonly ApplicationDbContext _dbContext;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            this._categoryRepository = categoryRepository;
+        }
+        public IActionResult Index()
+        {
+            List<Category>? category = _categoryRepository.GetAll().ToList();
+            return View(category);
+        }
+
+
+        //public IActionResult Edit()
+        //{
+        //    //List<Category>? category = _categoryRepository.Categories.ToList();
+        //    //return View(category);
+        //}
+
+
+        public IActionResult Create()
+        {
+            //List<Category>? category = _dbContext.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category name can-not be same as Display order");
+
+            }
+
+            if (obj.Name!= null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is not allowed");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _categoryRepository.Add(obj);
+                _categoryRepository.Save();
+                return RedirectToAction("Index");
+            }   return View();
+        }
+    }
+}
